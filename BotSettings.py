b836e2ccaa88f73f9317e2e278b8b07241834fdf
Bruggles718 <44073701+Bruggles718@users.noreply.github.com>
2020-11-04 19:05:38 -0500

    Update BotSettings.py

--- BotSettings.py
+++ BotSettings.py
@@ -2,4 +2,4 @@ HOST = "irc.twitch.tv"
 PORT = 6667
 PASS = "" # go to https://twitchapps.com/tmi/ while logged into your bot's account and put what it generates in the quotes here. Ex. oauth:FRc0oM0Kp0rthKwCzIjpjf2Hk0solj
 NICK = "" # put the name of the bot account here. make sure this is all lowercase, even if your bot has uppercase letters in it's name
-CHANNEL = "" # put the name of the channel you want the bot to join here
+CHANNEL = "" # put the name of the channel you want the bot to join here. make sure its all lowercase, same as the bot name
